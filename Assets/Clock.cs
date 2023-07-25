54262a3cb874fa61d36ce62277b1d8e782c8f961
unknown <kettunen.petri@gmail.com>
2023-07-25 14:32:17 +0300

    indicators

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -1,21 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Clock : MonoBehaviour
 {
 	[SerializeField]
-	private GameObject SecondsArmPivot, MinutesArmPivot, HoursArmPivot;
+	private GameObject secondsHandle, minutesHandle, hoursHandle;
 	private float secondsMultiplier = 1f;
 	private int inGameSeconds;
-	private int timeAtStart = 20000;
+	private int timeAtStart = 1;
 
     // Update is called once per frame
     void Update()
     {
         inGameSeconds = Mathf.RoundToInt(Time.time * secondsMultiplier) + timeAtStart;
-		SecondsArmPivot.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 60, 0);
-		MinutesArmPivot.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 3600, 0);
-		HoursArmPivot.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 43200, 0);
+		secondsHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 60, 0);
+		minutesHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 3600, 0);
+		hoursHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 43200, 0);
     }
+
 }
