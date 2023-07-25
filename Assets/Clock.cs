7c51e9e39e266a6ae2bb0918743244fd7610d9d4
unknown <kettunen.petri@gmail.com>
2023-07-25 15:37:09 +0300

    realistic seconds

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -9,15 +9,14 @@ public class Clock : MonoBehaviour
 	private GameObject secondsHandle, minutesHandle, hoursHandle;
 	private float secondsMultiplier = 1f;
 	private int inGameSeconds;
-	private int timeAtStart = 1;
 
     // Update is called once per frame
     void Update()
     {
-        inGameSeconds = Mathf.RoundToInt(Time.time * secondsMultiplier) + timeAtStart;
-		secondsHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 60, 0);
-		minutesHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 3600, 0);
-		hoursHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360 / 43200, 0);
+        inGameSeconds = Mathf.RoundToInt(Time.time * secondsMultiplier);
+		secondsHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360/60, 0);
+		minutesHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360/3600, 0);
+		hoursHandle.transform.localRotation = Quaternion.Euler(0, inGameSeconds * 360/43200, 0);
     }
 
 }
